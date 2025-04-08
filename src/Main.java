6a8c77d0010d2261a223dc94e56d619c7365f0ed
freshbeer12 <nekvoj13@gasos-ro.cz>
2025-04-08 07:55:12 +0200

    projekt telesa

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             ArrayList<Jehlan> jehlan = new ArrayList<Jehlan>();
             Scanner sc = new Scanner(System.in);
 
-            public static void main(String[] args) {
+             static void main(String[] args) {
                 int volba;
                 System.out.println("Vyberte jednu z možností : Přídat těleso (1) / Vypsat všechna tělesa jednoho druhu (2) / Těleso s největším a nejmenším povrchem (3) / Počet jednotlivých druhů těles v poli (4) / Součet všech objemů těles (5) / Krychli, kužel a jehlan s největším povrchem (6)");
                 volba = sc.nextInt();
@@ -22,20 +22,29 @@ public class Main {
                 }
                 while (volba != 0);
             }
-            public static void pridejTeleso() {
+             void pridejTeleso() {
                 System.out.println("Zvol, které těleso si přeješ přidat : Krychle (1) / Kuzel (2) / Jehlan (3)");
                 int druh = sc.nextInt();
                 if (druh == 1){
                     System.out.println("Zadej hranu krychle");
                     double hrana = sc.nextDouble();
                     krychle.add(new Krychle(hrana));
-                } else if (druh == 2) {
+                }
+                else if (druh == 2) {
                     System.out.println("Zadej poloměr podstavy");
                     double podstava = sc.nextDouble();
                     kuzel.add(new Kuzel(podstava));
                     System.out.println("Zadej výšku");
                     double vyska = sc.nextDouble();
                     kuzel.add(new Kuzel(vyska));
+                }
+                else if (druh == 3) {
+                    System.out.println("Zadej hranu dolní podstavy");
+                    double podstavaDP = sc.nextDouble();
+                    jehlan.add(new Jehlan(podstavaDP));
+                    System.out.println("Zadej výšku jehlanu");
+                    double vyskaJehlan = sc.nextDouble();
+                    jehlan.add(new Jehlan(vyskaJehlan));
                 }
             }
 
